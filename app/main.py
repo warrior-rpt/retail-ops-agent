b0c6810b7baa91099a7e4bebe13893c4b01a9f00
warrior-rpt <Rithin@MacBook-Pro.local>
2026-01-10 18:58:41 -0800

    Sns Notification added

--- app/main.py
+++ app/main.py
@@ -1,30 +1,32 @@
 from app.agent.graph import analyze_node, plan_node, act_node
 from app.memory.sales_data import SalesData
 from dataclasses import asdict
+from app.tools.notification import Notifier
 
 def handler(event, context=None):
     results = []
 
-    # Get all SKUs
     skus = SalesData.get_all_skus()
 
     for sku in skus:
         state = {}
         state["sku"] = sku
 
-        # ANALYZE
         state = analyze_node(state)
-
-        # PLAN
         state = plan_node(state)
-
-        # ACT
         state = act_node(state)
 
         results.append({
             "sku": sku,
-            "decision": asdict(state["final_decision"])  # Dataclass to dict
+            "decision": asdict(state["final_decision"])
         })
 
+    # Send summary notification
+    summary_msg = "\n".join(
+        [f"{r['sku']}: {r['decision']['decision']}" for r in results]
+    )
+    Notifier.send_email("Retail Ops Agent Decisions", summary_msg)
+
     return results
+
 
--- app/tools/notification.py
+++ app/tools/notification.py
@@ -1,3 +1,23 @@
 
-def notify_ops(message: str) -> None:
-    print(f"[NOTIFICATION SENT]: {message}")
+import os
+import boto3
+
+TOPIC_ARN = os.getenv("SNS_TOPIC_ARN")
+REGION = os.getenv("AWS_REGION")
+
+sns = boto3.client("sns", region_name=REGION)
+
+
+class Notifier:
+    @staticmethod
+    def send_email(subject: str, message: str):
+        if not TOPIC_ARN:
+            print("SNS_TOPIC_ARN not configured")
+            return
+
+        sns.publish(
+            TopicArn=TOPIC_ARN,
+            Subject=subject,
+            Message=message
+        )
+
